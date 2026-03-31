65dbe4204a625d4b6d6f42144c147649b9ecfba9
hsy1410-dev <hsy1410@gmail.com>
2026-03-31 18:40:06 +0900

    Update deleteUser.js

--- api/admin/deleteUser.js
+++ api/admin/deleteUser.js
@@ -17,8 +17,21 @@ if (!admin.apps.length) {
   });
 }
 
+function setCorsHeaders(res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Max-Age", "3600");
+}
+
 export default async function handler(req, res) {
+  setCorsHeaders(res);
+
   try {
+    if (req.method === "OPTIONS") {
+      return res.status(204).end();
+    }
+
     if (req.method !== "POST") {
       return res.status(405).json({ error: "Method Not Allowed" });
     }
